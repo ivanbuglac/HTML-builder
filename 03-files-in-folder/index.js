4973bf7ee7e5fde0161a2d9fea50a1daa1822bc9
Ivan <ivanbuglac@gmail.com>
2025-01-23 09:50:58 +0300

    fix: Add try/catch error handler to task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,22 +1,27 @@
-const { error } = require('console');
 const fs = require('fs/promises');
 const path = require('path');
 
 const secretFolderPath = path.join(__dirname, 'secret-folder');
 
 async function filesInSecretFolder() {
-  const files = await fs.readdir(secretFolderPath, { withFileTypes: true });
-  for (const file of files) {
-    if (file.isFile()) {
-      const filePath = path.join(secretFolderPath, file.name);
-      const infoAboutFile = await fs.stat(filePath);
-      const fileName = path.parse(file.name).name;
-      const fileExtension = path.extname(file.name).slice(1);
-      const fileSize = infoAboutFile.size;
-      console.log(`${fileName} - ${fileExtension} - ${fileSize} bytes`);
-    } else {
-      console.log(error);
+  try {
+    const files = await fs.readdir(secretFolderPath, { withFileTypes: true });
+    for (const file of files) {
+      try {
+        if (file.isFile()) {
+          const filePath = path.join(secretFolderPath, file.name);
+          const infoAboutFile = await fs.stat(filePath);
+          const fileName = path.parse(file.name).name;
+          const fileExtension = path.extname(file.name).slice(1);
+          const fileSize = infoAboutFile.size;
+          console.log(`${fileName} - ${fileExtension} - ${fileSize} bytes`);
+        }
+      } catch (innerError) {
+        console.error(`Error processing file ${file.name}:`, innerError.message);
+      }
     }
+  } catch (error) {
+    console.error('Error reading the secret folder:', error.message);
   }
 }
 
